05f179f9fdf7c550bd5b06360f4f5764bb22f4a9
agedus <bassemens@gmail.com>
2019-11-22 13:47:02 +0100

    ahoe

---
+++ func.py
@@ -0,0 +1,8 @@
+import subprocess
+import configparser
+
+config = configparser.ConfigParser()
+config.read('settings.ini')
+
+e_extension = ""
+
---
+++ host.py
@@ -0,0 +1,5 @@
+from flask import Flask, request, jsonify
+from func import *
+
+app = Flask(__name__)
+
